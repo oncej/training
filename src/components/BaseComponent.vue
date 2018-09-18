cca6b644f1aed3683998b2b52db31974df7347e1
layZ <zl_remix@163.com>
2018-09-18 20:03:48 +0800

    first commit

---
+++ src/components/BaseComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <fieldset>
+        <legend>{{title}}</legend>
+    </fieldset>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            title:'标题'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
